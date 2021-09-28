555277cf6a48ae2466204abb9d9ca212c7cb4189
Kaitlyn Gatineau <gati0010@algonquinlive.com>
2021-09-27 20:02:47 -0600

    Updated redundant code in MainActivity

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -20,47 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_grid);
 
-        Button button = (Button)findViewById(R.id.button_L02);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, getResources().
-                        getString(R.string.toast_text_L02), Toast.LENGTH_LONG).show();
+        Button button = findViewById(R.id.button_L02);
+        button.setOnClickListener(v -> Toast.makeText(MainActivity.this, getResources().
+                getString(R.string.toast_text_L02), Toast.LENGTH_LONG).show());
+
+        CheckBox checkBox = findViewById(R.id.checkbox_L02);
+        checkBox.setOnCheckedChangeListener((cb, b) -> {
+            String x;
+            if (b){
+                x = " on";
+            } else {
+                x = " off";
             }
-        });
+            Snackbar.make(checkBox, getResources()
+                    .getString(R.string.snackbar_checkbox_L02) + x, Snackbar.LENGTH_LONG)
+                    .setAction("Undo", click -> cb.setChecked(!b)).show();
 
-        CheckBox checkBox = (CheckBox)findViewById(R.id.checkbox_L02);
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                                                @Override
-                                                public void onCheckedChanged(CompoundButton cb, boolean b) {
-                                                    String x;
-                                                    if (b){
-                                                        x = " on";
-                                                    } else {
-                                                        x = " off";
-                                                    }
-                                                    Snackbar.make(checkBox, getResources()
-                                                            .getString(R.string.snackbar_checkbox_L02) + x, Snackbar.LENGTH_LONG)
-                                                            .setAction("Undo", click -> cb.setChecked(!b)).show();
-
-                                                }
-
-
-                                            }
+        }
         );
-        Switch switchLab02 = (Switch)findViewById(R.id.switch_L02);
-        switchLab02.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton cb, boolean b) {
-                String x;
-                if (b){
-                    x = " on";
-                } else {
-                    x = " off";
-                }
-                Snackbar.make(switchLab02, getResources().getString(R.string.snackbar_switch_L02) + x,
-                        Snackbar.LENGTH_LONG).setAction("Undo", click -> cb.setChecked((!b))).show();
+        Switch switchLab02 = findViewById(R.id.switch_L02);
+        switchLab02.setOnCheckedChangeListener((cb, b) -> {
+            String x;
+            if (b){
+                x = " on";
+            } else {
+                x = " off";
             }
+            Snackbar.make(switchLab02, getResources().getString(R.string.snackbar_switch_L02) + x,
+                    Snackbar.LENGTH_LONG).setAction("Undo", click -> cb.setChecked((!b))).show();
         });
 
     }
